5ae0b3fdd9218192669686ed48464cbfe04eae58
Yasmeen saleh <lap top@DESKTOP-40R4L6L>
2026-03-23 13:38:56 +0300

    solve 4 , 6

--- Eid Challenge/index.js
+++ Eid Challenge/index.js
@@ -8,7 +8,7 @@ let array=[
   { event:"scroll", time:3 },
   { event:"scroll", time:4 },
   { event:"click", time:5 },
-  {event:"hover",time:6}
+  { event:"hover", time:6 }
 ];
 
 function logsUserEvent(arr){
@@ -32,6 +32,7 @@ function catchTheTarget(arr,target){
     
 for(let i =0; i< arr.length;i++){
     let sum=0;
+    // عشان يبلش جمع من محل ما انا واقف 
     for(let j=i ; j< arr.length;j++){
        sum+=arr[j];
         if(sum === target){
@@ -51,7 +52,7 @@ console.log(catchTheTarget([1,4,20,3,10,5],33));
 console.log("------------Task 3-------------");
 function catchTheMaxProfit(arr){
      let minPrice = arr[0];
-        let maxProfit = 0;
+     let maxProfit = 0;
     for(let i =1;i<arr.length;i++){
        
         if(arr[i] < minPrice){
@@ -68,9 +69,34 @@ console.log(catchTheMaxProfit([7,1,5,3,6,4]));
 //Buildings have different heights, and when it rains, water gets trapped between them.
 //  You need to calculate how much water is stored.
 console.log("------------Task 4-------------");
+function trapWater(arr){
+    let leftMax =[];
+    let rightMax =[];
+    
+    let currentLeftMax=0
+for(let i=0; i<arr.length;i++){
+if(currentLeftMax < arr[i]){
+    currentLeftMax=arr[i]
+}
+leftMax[i]=currentLeftMax;
+}
+let currentRightMax = 0;
+for(let i=arr.length-1; i>=0;i--){
 
+if(currentRightMax < arr[i]){
+   currentRightMax=arr[i]
+}
+rightMax[i]=currentRightMax
+}
+let totalWater=0;
+for(let i =0;i<arr.length;i++){
+let min = Math.min(leftMax[i],rightMax[i]);
+totalWater += (min - arr[i]);
+}
+return totalWater;
+}
 
-console.log("not solved yet");
+console.log(trapWater([0,1,0,2,1,0,1,3,2,1,2,1]));
 //Task 5
 //A user browsing log is recorded. You need to find the longest continuous period where no value is repeated.
 console.log("------------Task 5-------------");
@@ -78,9 +104,10 @@ console.log("------------Task 5-------------");
 function  longestUniqueSubarray(arr){
 
 let maxLength=0;
- let current=[];
+
 for(let i=0;i<arr.length;i++){
-   
+    let current=[];
+    // برضو بدي ابلش مكان ما انا واقفة
 for(let j= i;j<arr.length ;j++){
     if(current.includes(arr[j])){
         break;
@@ -91,7 +118,7 @@ for(let j= i;j<arr.length ;j++){
     }
 }
 }
-return current;
+return maxLength;
 }
 
 console.log(longestUniqueSubarray([1,2,3,1,2,3,4,5]));
@@ -121,13 +148,55 @@ let leftProduct=1;
  for(let i=0;i<arr.length;i++){
      result[i] = leftProduct;
      leftProduct *=arr[i];
+     //result =[1,1,2,6] هيك بصير شكلها
  }
  let rightProduct =1;
  for(let i = arr.length-1;i>=0 ;i--){
     //بنضرب اليسار بالقيمة الي اصلا موجودة باليمين
+    // result =[24,12,4,4]بس من اليمين قبل ما اضربها
     result[i] *= rightProduct;
     rightProduct *=arr[i];
  }
  return result
 }
 console.log(ProductofArrayExceptSelf([1,2,3,4]));
+//Task 7
+//A robot moves in a spiral path inside a grid and collects numbers along the way.
+// ```
+// [
+//   [1,2,3],
+//   [4,5,6],
+//   [7,8,9]
+// ]
+// ```
+
+// 🎯 **Task:**
+
+// ```
+// [1,2,3,6,9,8,7,4,5]
+// ```
+console.log("------------Task 7-------------");
+function spiralPath(matrix) {
+ return `not solved yet`
+}
+
+console.log(spiralPath([
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9]
+])); 
+//Task 8
+//You have a list of purchased items, and a target list of required items.
+//  Find the smallest continuous part that contains all required items.
+// arr= [1,2,2,3,1,2,3]
+// target= [1,2,3]
+console.log("------------Task 8-------------");
+function minWindowSubarray(arr, target) {
+    let minLen = Infinity;
+    let result = [];
+
+
+    return `not solved yet`;
+}
+
+console.log(minWindowSubarray([1, 2, 2, 3, 1, 2, 3], [1, 2, 3]));
