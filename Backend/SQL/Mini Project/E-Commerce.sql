a52be3cfadc008587362acb67b759a810e71362d
YasmeenSaleh00 <yasmeensaleh147@gmail.com>
2026-04-11 16:31:10 +0300

    add e-commerce diagram

--- Backend/SQL/Mini Project/E-Commerce.sql
+++ Backend/SQL/Mini Project/E-Commerce.sql
@@ -506,4 +506,577 @@ CREATE PROCEDURE UpdateBrandStatus
     END;
     EXEC UpdateBrandStatus @BrandId = 1;
 --17 - Get ALL Brands
+CREATE PROCEDURE GetAllBrands
+AS
+BEGIN
+SELECT B.Id , B.Name , B.CreationDate , B.Image , U.FirstName +' ' +U.LastName  AS 'Created By'
+FROM Brands B JOIN Users U ON B.UserId = U.Id;
+END;
+EXEC GetAllBrands;
 --18 - Get Brand By Id 
+CREATE PROCEDURE GetBrandById
+@BrandId INT
+AS
+BEGIN
+SELECT B.Id , B.Name , B.CreationDate , B.Image , U.FirstName +' ' +U.LastName  AS 'Created By'
+FROM Brands B JOIN Users U ON B.UserId = U.Id
+WHERE B.Id = @BrandId;
+END;
+EXEC GetBrandById @BrandId = 1;
+-- 19 - Add Product 
+CREATE PROCEDURE AddProduct
+    @Name VARCHAR(50),
+    @Image VARCHAR(MAX),
+    @Price DECIMAL(10,2),
+    @Quantity INT,
+    @UserId INT,
+    @BrandId INT,
+    @CategoryId INT,
+    @ProductStatusId INT
+AS
+BEGIN
+    INSERT INTO Products (
+        [Name],
+        [Image],
+        [Price],
+        [Quantity],
+        [UserId],
+        [BrandId],
+        [CategoryId],
+        [ProductStatusId]
+    )
+    VALUES (
+        @Name,
+        @Image,
+        @Price,
+        @Quantity,
+        @UserId,
+        @BrandId,
+        @CategoryId,
+        @ProductStatusId
+    );
+END;
+EXEC AddProduct 
+    @Name = 'Samsung Galaxy S24 Ultra', 
+    @Image = 'https://example.com/images/s24-ultra.jpg', 
+    @Price = 1199.99, 
+    @Quantity = 25, 
+    @UserId = 1,        
+    @BrandId = 3,       
+    @CategoryId = 1,   
+    @ProductStatusId = 5; 
+-- 20 - Update Products
+CREATE PROCEDURE UpdateProduct
+@ProductId INT,
+@BrandId INT = NULL ,
+@Name VARCHAR(20) =NULL,
+@Image VARCHAR(MAX)=NULL ,
+@Price DECIMAL(10,2)=NULL,
+@Quantity INT = NULL,
+@CategoryId INT = NULL,
+@ProductStatusId INT = NULL
+AS
+BEGIN
+ UPDATE Products
+    SET 
+       Name = ISNULL(@Name, Name),
+        Image = ISNULL(@Image, Image),
+        Price = ISNULL(@Price, Price),
+        Quantity = ISNULL(@Quantity, Quantity),
+        BrandId = ISNULL(@BrandId, BrandId),
+        CategoryId = ISNULL(@CategoryId, CategoryId),
+        ProductStatusId = ISNULL(@ProductStatusId, ProductStatusId),
+        ModificationDate = GETDATE()
+        WHERE Id = @ProductId;
+END;
+EXEC UpdateProduct @ProductId =1 , @Image='https://shophuna.com/cdn/shop/files/GalaxyS24Ultratitaniumgray_2_253590c1-26ac-4e70-8f1d-493c391fa4bf.jpg?v=1760999583&width=823';
+--21 - GET ALL PRODUCT WITH CATEGORY AND BRAND AND WHO CREATED THEM
+CREATE PROCEDURE GetAllProducts
+AS
+BEGIN
+SELECT P.Id , P.Name AS 'Product Name' , P.Quantity , P.Image ,P.CreationDate , 
+P.Price ,L.Value AS 'Product Status', C.Name AS 'Category',B.Name AS 'Brand'  , U.FirstName+' '+U.LastName 
+AS 'Created By'  
+FROM Products P JOIN Categories C  ON P.CategoryId = C.Id
+JOIN Brands B ON B.Id = P.BrandId
+JOIN Users U ON U.Id = P.UserId 
+JOIN LookupItems L ON L.Id = P.ProductStatusId
+END;
+--22 - GET  PRODUCT BY ID WITH CATEGORY AND BRAND AND WHO CREATED THEM
+CREATE PROCEDURE GetProductById
+@ProductId INT
+AS
+BEGIN
+SELECT P.Id , P.Name AS 'Product Name' , P.Quantity , P.Image ,P.CreationDate , 
+P.Price ,L.Value AS 'Product Status', C.Name AS 'Category',B.Name AS 'Brand'  , U.FirstName+' '+U.LastName 
+AS 'Created By'  
+FROM Products P JOIN Categories C  ON P.CategoryId = C.Id
+JOIN Brands B ON B.Id = P.BrandId
+JOIN Users U ON U.Id = P.UserId 
+JOIN LookupItems L ON L.Id = P.ProductStatusId
+WHERE P.Id = @ProductId;
+END;
+EXEC GetProductById @ProductId =1;
+-- 23 -APLAY SOFT DELET ON PRODUCT
+CREATE PROCEDURE UpdateProductStatus
+@ProductId INT
+AS
+BEGIN
+UPDATE Products
+SET IsDeleted = 1 , ProductStatusId=6,
+ModificationDate =GETDATE()
+WHERE Id = @ProductId;
+END;
+EXEC UpdateProductStatus @ProductId = 1;
+-- 24 - CREATE ACCOUNT CUSTOMER
+CREATE PROCEDURE RegisterCustomer
+    @FirstName VARCHAR(20),
+    @LastName VARCHAR(20),
+    @Email VARCHAR(100),
+    @Phone VARCHAR(14),
+    @Password VARCHAR(10), 
+    @GenderId INT
+AS
+BEGIN
+    INSERT INTO Customers (
+        [FirstName],
+        [LastName],
+        [Email],
+        [Phone],
+        [Password],
+        [CustomerStatusId],
+        [GenderId]
+    )
+    VALUES (
+        @FirstName,
+        @LastName,
+        @Email,
+        @Phone,
+        @Password,
+        3,
+        @GenderId
+    );
+END;
+EXEC RegisterCustomer 
+    @FirstName = 'Ahmad', 
+    @LastName = 'Hassan', 
+    @Email = 'ahmad.h@gmail.com', 
+    @Phone = '0795554433', 
+    @Password = 'Ah33@123', 
+    @GenderId = 1; 
+EXEC RegisterCustomer 
+    @FirstName = 'Omar', 
+    @LastName = 'Zaid', 
+    @Email = 'omar.zaid@outlook.com', 
+    @Phone = '0780112233', 
+    @Password = 'OmR#2026', 
+    @GenderId = 1; 
+EXEC RegisterCustomer 
+    @FirstName = 'Sara', 
+    @LastName = 'Mansour', 
+    @Email = 'sara.m99@gmail.com', 
+    @Phone = '0795566778', 
+    @Password = 'Sara!8888', 
+    @GenderId = 2; 
+EXEC RegisterCustomer 
+    @FirstName = 'Khaled', 
+    @LastName = 'Salem', 
+    @Email = 'khaled.dev@yahoo.com', 
+    @Phone = '0770998877', 
+    @Password = 'Khl@57772', 
+    @GenderId = 1;
+    --25 - GET ALL Users WHO IS ACTIVE
+CREATE PROCEDURE GetAllCustomers
+AS
+BEGIN 
+SELECT C.Id, C.FirstName + ' ' + C.LastName AS 'Full Name ', C.Email , C.Phone , LI.Value AS ' Gender' ,
+L.Value AS 'Customer Status '
+FROM Customers C 
+JOIN LookupItems L ON C.CustomerStatusId = L.Id
+JOIN LookupItems LI ON C.GenderId = LI.Id
+END;
+EXEC GetAllCustomers;
+-- 26 - GET ALL CUSTOMERS WHO IS ACTIVE
+CREATE PROCEDURE GetAllCustomersWhoIsActive 
+AS
+BEGIN 
+SELECT C.Id, C.FirstName + ' ' + C.LastName AS 'Full Name ', C.Email , C.Phone , LI.Value AS ' Gender' ,
+L.Value AS 'Customer Status '
+FROM Customers C 
+JOIN LookupItems L ON C.CustomerStatusId = L.Id
+JOIN LookupItems LI ON C.GenderId = LI.Id
+WHERE C.CustomerStatusId = 3 AND C.IsDeleted = 0;
+END;
+EXEC GetAllCustomersWhoIsActive ;
+-- 27 - BAN CUSTOMERS
+CREATE PROCEDURE BanCustomer
+    @CustomerId INT
+AS
+BEGIN
+    UPDATE Customers
+    SET 
+        CustomerStatusId = 4,   
+        IsDeleted = 1,         
+        ModificationDate = GETDATE() 
+    WHERE Id = @CustomerId;
+END;
+EXEC BanCustomer @CustomerId = 2;
+-- 28 - GET ALL CUSTOMERS WHO IS banned
+CREATE PROCEDURE GetAllCustomersWhoIsBanned
+AS
+BEGIN 
+SELECT C.Id, C.FirstName + ' ' + C.LastName AS 'Full Name ', C.Email , C.Phone , LI.Value AS ' Gender' ,
+L.Value AS 'Customer Status '
+FROM Customers C 
+JOIN LookupItems L ON C.CustomerStatusId = L.Id
+JOIN LookupItems LI ON C.GenderId = LI.Id
+WHERE C.CustomerStatusId = 4 AND C.IsDeleted = 1;
+END;
+EXEC GetAllCustomersWhoIsBanned ;
+-- 29 - Get Customer by id
+CREATE PROCEDURE GetCustomerById
+@CustomerId INT
+AS
+BEGIN 
+SELECT C.Id, C.FirstName + ' ' + C.LastName AS 'Full Name ', C.Email , C.Phone , LI.Value AS ' Gender' ,
+L.Value AS 'Customer Status '
+FROM Customers C 
+JOIN LookupItems L ON C.CustomerStatusId = L.Id
+JOIN LookupItems LI ON C.GenderId = LI.Id
+WHERE C.Id = @CustomerId;
+END;
+EXEC GetCustomerById @CustomerId = 3;
+-- 30 - Update Customer Profile
+CREATE PROCEDURE UpdateCustomerProfile
+    @CustomerId INT,
+    @FirstName VARCHAR(20) = NULL,
+    @LastName VARCHAR(20) = NULL,
+    @Phone VARCHAR(14) = NULL,
+    @Password VARCHAR(MAX) = NULL
+AS
+BEGIN
+    UPDATE Customers
+    SET 
+        FirstName = ISNULL(@FirstName, FirstName),
+        LastName = ISNULL(@LastName, LastName),
+        Phone = ISNULL(@Phone, Phone),
+        [Password] = ISNULL(@Password, [Password]),
+        ModificationDate = GETDATE() 
+    WHERE Id = @CustomerId 
+    AND IsDeleted = 0;
+END;
+EXEC UpdateCustomerProfile @CustomerId = 3, @FirstName = ' New';
+--31- Add product to CART
+CREATE PROCEDURE AddToCart
+    @CustomerId INT,
+    @ProductId INT,
+    @Quantity INT
+AS
+BEGIN
+    DECLARE @CartId INT;
+    SELECT @CartId = Id FROM Cart WHERE CustomerId = @CustomerId;
+    IF @CartId IS NULL
+    BEGIN
+        INSERT INTO Cart (CustomerId) 
+        VALUES (@CustomerId);
+        
+        SET @CartId = SCOPE_IDENTITY();
+    END
+    IF EXISTS (SELECT 1 FROM CartItems WHERE CartId = @CartId AND ProductId = @ProductId)
+    BEGIN
+        UPDATE CartItems 
+        SET Quantity = Quantity + @Quantity 
+        WHERE CartId = @CartId AND ProductId = @ProductId;
+    END
+    ELSE
+    BEGIN
+        INSERT INTO CartItems (CartId, ProductId, Quantity)
+        VALUES (@CartId, @ProductId, @Quantity);
+    END
+END;
+EXEC AddToCart @CustomerId = 3, @ProductId = 1, @Quantity = 1;
+EXEC AddToCart @CustomerId = 3, @ProductId = 1, @Quantity = 2;
+EXEC AddToCart @CustomerId = 4, @ProductId = 1, @Quantity = 2;
+EXEC AddToCart @CustomerId = 4, @ProductId = 5, @Quantity = 4;
+
+-- 32 - GET all PRODUCTs IN CARTITEMS
+CREATE PROCEDURE GetAllProductsInCart
+    @CartId INT 
+AS
+BEGIN
+    SELECT 
+        Cu.FirstName + ' ' + Cu.LastName AS 'Customer Name',
+        Cu.Email, 
+        Cu.Phone,
+        P.Name AS 'Product Name', 
+        P.Price AS 'Unit Price',
+        CI.Quantity, 
+        (CI.Quantity * P.Price) AS 'Total Price'
+    FROM CartItems CI
+    JOIN Cart C ON CI.CartId = C.Id            
+    JOIN Customers Cu ON Cu.Id = C.CustomerId
+    JOIN Products P ON P.Id = CI.ProductId
+    WHERE C.Id = @CartId;                     
+END;
+EXEC GetAllProductsInCart @CartId =2;
+-- 33- update Quantity in cartItem
+CREATE PROCEDURE UpdateQuantityOfProductInCartItem
+@CartId INT ,
+@ProductId INT,
+@Quantity INT
+AS
+BEGIN
+IF @Quantity <= 0
+BEGIN 
+DELETE FROM CartItems
+WHERE ProductId = @ProductId AND CartId = @CartId
+END;
+ELSE
+BEGIN
+UPDATE CartItems
+SET Quantity = @Quantity
+WHERE CartId = @CartId AND ProductId = @ProductId
+END;
+END;
+EXEC UpdateQuantityOfProductInCartItem @CartId =1 , @ProductId=1 , @Quantity=0;
+EXEC UpdateQuantityOfProductInCartItem @CartId =2 , @ProductId=1 , @Quantity=4;
+--34 - delete product from cartitem
+CREATE PROCEDURE DeleteProductFromCart
+@CartId INT ,
+@ProductId INT
+AS
+BEGIN
+DELETE FROM CartItems 
+WHERE CartId =@CartId AND ProductId = @ProductId 
+END;
+EXEC DeleteProductFromCart @CartId = 2 , @ProductId = 5;
+-- 35 - Add to wishlist
+CREATE PROCEDURE AddToWishList
+    @ProductId INT,
+    @CustomerId INT
+AS
+BEGIN
+    DECLARE @WishlistId INT;
+    SELECT @WishlistId = Id FROM Wishlist WHERE CustomerId = @CustomerId;
+    IF @WishlistId IS NULL
+    BEGIN
+        INSERT INTO Wishlist(CustomerId) 
+        VALUES (@CustomerId);
+        
+        SET @WishlistId = SCOPE_IDENTITY();
+    END
+    IF NOT EXISTS (SELECT 1 FROM WishlistItems WHERE WishlistId = @WishlistId AND ProductId = @ProductId)
+    BEGIN
+        INSERT INTO WishlistItems(WishlistId, ProductId)
+        VALUES (@WishlistId, @ProductId);
+    END
+END;
+EXEC AddToWishList @ProductId = 5,@CustomerId = 5;
+-- 36 GetAllProductInWishlist
+CREATE PROCEDURE GetAllProductInWishlist
+@WishListId INT
+AS 
+BEGIN
+SELECT C.Id , C.FirstName , C.Email , C.Phone , P.Name AS 'Product Name ' , P.Price
+FROM Wishlist W 
+JOIN WishlistItems WI on W.Id = WI.WishlistId
+JOIN Products P ON WI.ProductId = P.Id
+JOIN Customers C ON C.Id = W.CustomerId
+WHERE W.Id = @WishListId
+END;
+EXEC GetAllProductInWishlist @WishListId = 1;
+-- 37 CREATEOrder
+CREATE PROCEDURE CreateOrder
+    @ShippingAddress VARCHAR(100),
+    @Note VARCHAR(MAX),
+    @CustomerId INT,
+    @PaymentMethodId INT
+AS
+BEGIN
+    DECLARE @CalculatedTotal DECIMAL(18,2);
+    DECLARE @CartId INT;
+    DECLARE @OrderId INT;
+    SELECT @CartId = Id FROM Cart WHERE CustomerId = @CustomerId;
+    SELECT @CalculatedTotal = SUM(CI.Quantity * P.Price)
+    FROM CartItems CI
+    JOIN Products P ON CI.ProductId = P.Id
+    WHERE CI.CartId = @CartId;
+    INSERT INTO [Orders] 
+    (TotalPrice, 
+    ShippingAddres, 
+    Note, 
+    DeliveryDATE, 
+    OrderStatusId, 
+    PaymentMethodId, 
+    PaymentStatusId, 
+    CustomerId)
+    VALUES (
+    @CalculatedTotal,
+    @ShippingAddress,
+    @Note, DATEADD(DAY, 3, GETDATE()),
+     7, 
+     @PaymentMethodId,
+     15, 
+     @CustomerId);
+    SET @OrderId = SCOPE_IDENTITY();
+    INSERT INTO OrderItems (OrderId, ProductId, Quantity,UnitPrice )
+    SELECT @OrderId, CI.ProductId, CI.Quantity, P.Price
+    FROM CartItems CI
+    JOIN Products P ON CI.ProductId = P.Id
+    WHERE CI.CartId = @CartId;
+
+    DELETE FROM CartItems WHERE CartId = @CartId;
+
+END;
+EXEC CreateOrder 
+    @ShippingAddress = '123 Cyberpunk Blvd, Apartment 4B, Amman', 
+    @Note = 'Please leave it at the reception, handle with care.', 
+    @CustomerId = 4, 
+    @PaymentMethodId = 12;
+-- 38 GET ORDER DETIALS
+CREATE PROCEDURE GetOrderDetailsById
+@OrderId INT
+AS
+BEGIN
+SELECT C.FirstName + ' ' + C.LastName AS 'Customer Name' , C.Phone , C.Email , O.ShippingAddres ,P.Name AS 'Product Name' ,
+OI.Quantity , P.Price AS 'Price Per Items' ,L.Value AS 'Payment Method ' , LI.Value AS 'Order Status ',LIT.Value AS 'Payment Status ', O.TotalPrice 
+FROM Customers C 
+JOIN Orders O ON C.Id = O.CustomerId
+JOIN OrderItems OI ON O.Id = OI.OrderId
+JOIN Products P ON OI.ProductId = P.Id
+JOIN LookupItems L ON O.PaymentMethodId = L.Id
+JOIN LookupItems LI ON O.OrderStatusId = LI.Id
+JOIN LookupItems LIT ON O.PaymentStatusId = LIT.Id
+WHERE O.Id = @OrderId
+END;
+EXEC GetOrderDetailsById @OrderId = 1;
+-- 39 -- update OrderStatus
+CREATE PROCEDURE UpdateOrderStatus
+    @OrderId INT,
+    @NewStatusId INT
+AS
+BEGIN
+        UPDATE Orders
+        SET OrderStatusId = @NewStatusId,
+            ModificationDate = GETDATE() 
+        WHERE Id = @OrderId;    
+END;
+EXEC UpdateOrderStatus @OrderId = 1,@NewStatusId = 9; 
+-- 40 - add review
+CREATE PROCEDURE AddProductReview
+    @Rating INT,
+    @Comment VARCHAR(MAX),
+    @ProductId INT,
+    @CustomerId INT
+AS
+BEGIN
+    
+
+    INSERT INTO [dbo].[Reviews]
+           ([Rating]
+           ,[Comment]
+           ,[ProductId]
+           ,[CustomerId])
+     VALUES
+           (@Rating,
+            @Comment,
+            @ProductId,
+            @CustomerId);
+
+    
+END;
+EXEC AddProductReview 
+    @Rating = 5, 
+    @Comment = 'Amazing RGB lighting! Fits perfectly with my neon setup.', 
+    @ProductId = 5, 
+    @CustomerId = 5;
+    -- 41 GETALL REVIEW
+    CREATE PROCEDURE GetAllReviews
+AS
+BEGIN
+    SELECT 
+        R.Id AS ReviewId,
+        P.Name AS ProductName,
+        C.FirstName + ' ' + C.LastName AS CustomerName,
+        R.Rating,
+        R.Comment,
+        R.CreationDate
+    FROM Reviews R
+    JOIN Products P ON R.ProductId = P.Id
+    JOIN Customers C ON R.CustomerId = C.Id
+    ORDER BY R.CreationDate DESC; 
+END;
+EXEC GetAllReviews;
+-- 42 GET PRODUCT WITH PRICE RANGE
+CREATE PROCEDURE GetProductWithinRange
+@MinPrice DECIMAL ,
+@MaxPrice DECIMAL 
+AS
+BEGIN
+SELECT Name , Price  FROM Products  
+WHERE Price BETWEEN @MinPrice AND @MaxPrice
+ORDER BY Price ASC;
+END;
+EXEC GetProductWithinRange 1200 , 1500;
+
+--43 GET RECENT 5 ORDER
+CREATE PROCEDURE GetLastFiveOrder
+AS
+BEGIN
+SELECT TOP 5 C.FirstName + ' ' + C.LastName AS 'Customer Name' , C.Phone , C.Email , O.ShippingAddres,
+O.CreationDate,L.Value AS 'Payment Method ' , LI.Value AS 'Order Status ',LIT.Value AS 'Payment Status ', O.TotalPrice 
+FROM Orders O
+JOIN Customers C ON C.Id = O.CustomerId
+JOIN LookupItems L ON O.PaymentMethodId = L.Id
+JOIN LookupItems LI ON O.OrderStatusId = LI.Id
+JOIN LookupItems LIT ON O.PaymentStatusId = LIT.Id
+ORDER BY O.CreationDate DESC;
+END;
+EXEC GetLastFiveOrder;
+-- 44 -GetTotalSalesPerCustomer
+CREATE PROCEDURE GetTotalSalesPerCustomer
+AS
+BEGIN
+SELECT 
+    C.FirstName + ' ' + C.LastName AS [Customer Name],
+    COUNT(O.Id) AS [Total Orders],        
+    SUM(O.TotalPrice) AS [Total Spent]    
+FROM Customers C
+JOIN Orders O ON C.Id = O.CustomerId
+GROUP BY C.Id, C.FirstName, C.LastName         
+ORDER BY [Total Spent] DESC;        
+END;
+
+EXEC GetTotalSalesPerCustomer;
+-- 45 GetAvailableProducts
+CREATE PROCEDURE GetAvailableProducts
+AS
+BEGIN
+    SELECT 
+        Id, 
+        Name, 
+        Price, 
+        Image
+    FROM Products
+    WHERE IsDeleted = 0
+    ORDER BY Price ASC; 
+END;
+EXEC GetAvailableProducts;
+-- 46 GetProductsWithAverageRating
+CREATE PROCEDURE GetProductsWithAverageRating
+AS
+BEGIN
+    SELECT 
+        P.Id,
+        P.Name,
+        P.Price,
+        AVG(R.Rating) AS [Average Rating], 
+        COUNT(R.Id) AS [Reviews Count] 
+    FROM Products P
+    LEFT JOIN Reviews R ON P.Id = R.ProductId AND R.IsDeleted = 0
+    WHERE P.IsDeleted = 0
+    GROUP BY P.Id, P.Name, P.Price
+    ORDER BY [Average Rating] DESC; 
+END;
+EXEC GetProductsWithAverageRating;
+
